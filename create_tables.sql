cb044cbabee77ee081114b64a97885ef3eed2d52
vahidzee <vahid98zee@gmail.com>
2020-01-29 16:01:39 +0330

    debug table creations

--- create_tables.sql
+++ create_tables.sql
@@ -1,20 +1,21 @@
-
-/* User table */
-create table if not exists Usr (
-    id          SERIAL,
-    user_name   char(30) UNIQUE,
-    first_name  char(30) not null,
-    last_name   char(40),
-    biography   char(300),
-    is_bot      bool not null default true,
+-- User table
+create table if not exists Usr
+(
+    id         SERIAL,
+    user_name  char(30) UNIQUE,
+    first_name char(30) not null,
+    last_name  char(40),
+    biography  char(300),
+    is_bot     bool     not null default true,
     PRIMARY KEY (id)
 );
 
 -- Person Table
-create table if not exists Person (
-    id          integer not null UNIQUE ,
-    phone       char(30),
-    last_seen   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+create table if not exists Person
+(
+    id        integer not null UNIQUE,
+    phone     char(30),
+    last_seen TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
@@ -22,12 +23,13 @@ create table if not exists Person (
 );
 
 -- Bot Table
-create table if not exists Bot (
-    id          integer not null UNIQUE,
-    creator     integer not null,
+create table if not exists Bot
+(
+    id           integer not null UNIQUE,
+    creator      integer not null,
     access_token char(512) UNIQUE,
-    webhook_url text UNIQUE,
-    uses_poll   boolean not null default true,
+    webhook_url  text UNIQUE,
+    uses_poll    boolean not null default true,
     FOREIGN KEY (id) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
@@ -39,20 +41,22 @@ create table if not exists Bot (
 
 
 -- Chat Table
-create table if not exists Chat (
-    id          SERIAL,
-    chat_type   char(1) not null,    --P for private and C for Channel and G for groups
+create table if not exists Chat
+(
+    id        SERIAL,
+    chat_type char(1) not null, --P for private and C for Channel and G for groups
     PRIMARY KEY (id)
 );
 
 -- Group or Channel Table
-create table if not exists GroupChannel (
-    id          integer not null unique,
-    creator     integer not null unique,
+create table if not exists GroupChannel
+(
+    id          integer  not null unique,
+    creator     integer  not null unique,
     title       char(60) not null,
     description text,
     is_private  bool,
-    inv_link    text not null unique,
+    inv_link    text     not null unique,
     user_name   char(60),
     FOREIGN KEY (id) references Chat (id)
         ON DELETE CASCADE
@@ -64,16 +68,17 @@ create table if not exists GroupChannel (
 );
 
 -- Administrator Table
-create table if not exists Administrator (
-    chat        integer not null,
-    usr         integer not null ,
-    promoted_by integer,
-    send_messages bool not null default true,
-    delete_messages bool not null default true,
-    ban bool not null default true,
-    add_members bool not null default true,
-    add_admins bool not null default true,
-    update_info bool not null default true,
+create table if not exists Administrator
+(
+    chat            integer not null,
+    usr             integer not null,
+    promoted_by     integer,
+    send_messages   bool    not null default true,
+    delete_messages bool    not null default true,
+    ban             bool    not null default true,
+    add_members     bool    not null default true,
+    add_admins      bool    not null default true,
+    update_info     bool    not null default true,
     FOREIGN KEY (chat) references Chat (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
@@ -83,17 +88,18 @@ create table if not exists Administrator (
     FOREIGN KEY (promoted_by) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    PRIMARY KEY (chat,usr)
+    PRIMARY KEY (chat, usr)
 );
 
 -- File Table
-create table if not exists File (
+create table if not exists File
+(
     id          SERIAL,
     uploader    integer not null,
     file_name   char(256),
     caption     text,
-    file_type   char,   -- "I" for image, "A" for audio, "V" for video, "D" for document, "O" for others
-    address     text not null,
+    file_type   char, -- "I" for image, "A" for audio, "V" for video, "D" for document, "O" for others
+    address     text    not null,
     upload_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (uploader) references Usr (id)
         ON DELETE CASCADE
@@ -102,12 +108,13 @@ create table if not exists File (
 );
 
 -- Member Table
-create table if not exists Member (
-    usr     integer not null,
-    chat    integer not null,
-    start_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    added_by   integer,
-    last_read_message   integer,
+create table if not exists Member
+(
+    usr               integer not null,
+    chat              integer not null,
+    start_date        TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    added_by          integer,
+    last_read_message integer,
     FOREIGN KEY (usr) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
@@ -117,31 +124,32 @@ create table if not exists Member (
     FOREIGN KEY (chat) references Chat (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    UNIQUE (usr,chat),
-    PRIMARY KEY (usr,chat)
+    UNIQUE (usr, chat),
+    PRIMARY KEY (usr, chat)
 );
 
 -- Message Table
-create table if not exists Message (
-    destination integer,
-    id    integer,
-    forwarded_source    integer,
-    forwarded_id        integer,
-    reply_to_chat integer,
-    reply_to_id integer,
-    from_usr integer,
-    attachment integer,
-    upload_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    edit_date timestamp,
-    text text,
-    seen_count integer default 0,
+create table if not exists Message
+(
+    destination      integer not null,
+    id               integer not null,
+    forwarded_source integer,
+    forwarded_id     integer,
+    reply_to_chat    integer,
+    reply_to_id      integer,
+    from_usr         integer,
+    attachment       integer,
+    upload_date      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    edit_date        timestamp,
+    text             text,
+    seen_count       integer                  default 0,
     FOREIGN KEY (destination) references Chat (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (forwarded_source, forwarded_id) references Message (destination,id)
+    FOREIGN KEY (forwarded_source, forwarded_id) references Message (destination, id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (reply_to_chat, reply_to_id) references Message (destination,id)
+    FOREIGN KEY (reply_to_chat, reply_to_id) references Message (destination, id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     FOREIGN KEY (from_usr) references Usr (id)
@@ -150,32 +158,35 @@ create table if not exists Message (
     FOREIGN KEY (attachment) references File (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    UNIQUE (destination,id),
-    PRIMARY KEY (destination,id)
+    UNIQUE (destination, id),
+    PRIMARY KEY (destination, id)
 );
 
 -- Pinned message Table
-create table if not exists PinnedMessage (
-    chat        integer not null ,
-    usr         integer not null,
-    message     integer not null,
-    pin_date    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
-    is_pinned   bool not null,
+create table if not exists PinnedMessage
+(
+    chat         integer not null,
+    usr          integer not null,
+    message_chat integer not null,
+    message_id   integer not null,
+    pin_date     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
+    is_pinned    bool    not null         default true,
     FOREIGN KEY (chat, usr) references Administrator (chat, usr)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (message) references Message (id)
+    FOREIGN KEY (message_chat, message_id) references Message (destination, id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    UNIQUE (chat, usr, message, pin_date),
-    PRIMARY KEY (chat, usr, message, pin_date)
+    UNIQUE (chat, usr, message_id, pin_date),
+    PRIMARY KEY (chat, usr, message_id, pin_date)
 );
 
 -- Group Channel Picture Table
-create table if not exists GroupChannelPicture (
-    admin       integer not null,
-    chat        integer not null,
-    image       integer not null,
+create table if not exists GroupChannelPicture
+(
+    admin       integer                                            not null,
+    chat        integer                                            not null,
+    image       integer                                            not null,
     change_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
     FOREIGN KEY (chat, admin) references Administrator (chat, usr)
         ON DELETE CASCADE
@@ -188,9 +199,10 @@ create table if not exists GroupChannelPicture (
 );
 
 -- Profile Picture Table
-create table if not exists ProfilePicture (
-    usr         integer not null,
-    image       integer not null,
+create table if not exists ProfilePicture
+(
+    usr         integer                                            not null,
+    image       integer                                            not null,
     change_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP not null,
     FOREIGN KEY (usr) references Usr (id)
         ON DELETE CASCADE
@@ -203,10 +215,11 @@ create table if not exists ProfilePicture (
 );
 
 -- Banned Table
-create table if not exists Banned (
-    admin       integer not null,
-    chat        integer not null,
-    usr         integer not null,
+create table if not exists Banned
+(
+    admin integer not null,
+    chat  integer not null,
+    usr   integer not null,
     FOREIGN KEY (usr) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
@@ -218,9 +231,10 @@ create table if not exists Banned (
 );
 
 -- Block Table
-create table if not exists Block (
-    blocker     integer not null,
-    blockee     integer not null,
+create table if not exists Block
+(
+    blocker integer not null,
+    blockee integer not null,
     FOREIGN KEY (blockee) references Usr (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
