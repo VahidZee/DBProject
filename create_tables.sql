37e411ecae6235e53c24cae67a3bcd32ee9eb227
arvinAM17 <arvinazarmina@gmail.com>
2020-01-29 14:20:57 +0330

    Creating chat, person, bot, user, group tables.

--- create_tables.sql
+++ create_tables.sql
@@ -1,19 +1,19 @@
 
--- User table
+/* User table */
 create table if not exists Usr (
-    id   SERIAL,
-    user_name char(30) UNIQUE,
+    id          SERIAL,
+    user_name   char(30) UNIQUE,
     first_name  char(30) not null,
-    last_name char(40),
+    last_name   char(40),
     biography   char(300),
-    is_bot bool not null default true,
+    is_bot      bool not null default true,
     PRIMARY KEY (id)
 );
 
--- Person Table 
+-- Person Table
 create table if not exists Person (
-    id  integer not null UNIQUE ,
-    phone_number char(30),
+    id          integer not null UNIQUE ,
+    phone       char(30),
     last_seen   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id) references Usr (id)
         ON DELETE CASCADE
@@ -23,12 +23,38 @@ create table if not exists Person (
 
 -- Bot Table
 create table if not exists Bot (
-    id  integer not null UNIQUE,
-    creator  integer not null,
+    id          integer not null UNIQUE,
+    creator     integer not null,
     access_token char(512) UNIQUE,
     webhook_url text UNIQUE,
-    uses_polling boolean not null default true,
+    uses_poll   boolean not null default true,
     FOREIGN KEY (id) references Usr (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (creator) references Person (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    PRIMARY KEY (id)
+);
+
+
+-- Chat Table
+create table if not exists Chat (
+    id          SERIAL,
+    chat_type   char(1) not null,    --P for private and C for Channel and G for groups
+    PRIMARY KEY (id)
+);
+
+-- Group or Channel Table
+create table if not exists GroupChannel (
+    id          integer not null unique,
+    creator     integer not null unique,
+    title       char(60) not null,
+    description text(300),
+    is_private  bool,
+    inv_link    text(30) not null unique,
+    user_name   char(60),
+    FOREIGN KEY (id) references Chat (id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     FOREIGN KEY (creator) references Person (id)
