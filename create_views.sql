2115fd98a0597dc7fa3a97a0bd75c34a85aff88f
vahidzee <vahid98zee@gmail.com>
2020-01-30 13:57:12 +0330

    create UserPerson & UserBot views

---
+++ create_views.sql
@@ -0,0 +1,12 @@
+-- Person View
+create or replace view UserPerson as
+select usr.id, usr.user_name, p.phone_number, usr.first_name, usr.last_name,usr.biography, p.last_seen
+from usr inner join person p on usr.id = p.id
+where usr.is_bot = false;
+
+
+-- Bot View
+create or replace view UserBot as
+select usr.id, usr.user_name, usr.first_name, usr.last_name,usr.biography, b.creator as creator_id, b.access_token , b.webhook_url, b.uses_polling
+from usr inner join bot b on usr.id = b.id
+where usr.is_bot = true
