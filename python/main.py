43fbb563b5e0f356d441090d137ff75303edf2f3
vahidzee <vahid98zee@gmail.com>
2020-01-31 18:39:10 +0330

    add send message

--- python/main.py
+++ python/main.py
@@ -149,17 +149,25 @@ def handle_delete_me():
 
 
 # chats menu commands
+def handle_join_chat():
+    pass
+
+
 def get_chat_type(chat_id):
     cur.execute(f"SELECT chat_type from chat WHERE id={chat_id}")
     t = cur.fetchone()[0]
     return PRIVATE if t == 'P' else GROUP if t == 'G' else CHANNEL if t == 'C' else CHAT_ERROR
 
 
+def get_user_info(uid):
+    cur.execute(f"SELECT * from usr where id = {uid}")
+    return cur.fetchone()
+
+
 def get_pv_member(chat_id):
     cur.execute(f"SELECT usr from member WHERE usr != {user_id} and chat = {chat_id}")
     member_id = cur.fetchone()[0]
-    cur.execute(f"SELECT * from usr where id = {member_id}")
-    member_tuple = cur.fetchone()
+    member_tuple = get_user_info(member_id)
     return f'{member_tuple[2]} {member_tuple[3] or ""}'
 
 
@@ -167,6 +175,14 @@ def get_group_channel_title(chat_id):
     cur.execute(f"SELECT title from groupchannel where id = {chat_id}")
     title = cur.fetchone()[0]
     return f'{title}'
+
+
+def get_chat_title(chat_id, chat_type=None):
+    if not chat_type:
+        chat_type = get_chat_type(chat_id)
+    if chat_type == PRIVATE:
+        return get_pv_member(chat_id)
+    return get_group_channel_title(chat_id)
 
 
 def handle_show_my_chats():
@@ -190,23 +206,17 @@ def handle_show_my_chats():
     print('-\tPrivate Chats:')
     if private_chat_id:
         for id in private_chat_id:
-            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_pv_member(id), '\033[0m')
-    else:
-        print("\tNo private chats")
+            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_chat_title(id, PRIVATE), '\033[0m')
 
     print('-\tGroup Chats:')
     if group_id:
         for id in group_id:
-            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_group_channel_title(id), '\033[0m')
-    else:
-        print("\tNo group chats")
+            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_chat_title(id, GROUP), '\033[0m')
 
     print('-\tChannel Chats:')
     if channel_id:
         for id in channel_id:
-            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_group_channel_title(id), '\033[0m')
-    else:
-        print("\tNot subscribed in any channels yet :)")
+            print('\t\033[1;34m', str(id), '\033[0m\t-\t\033[1;34m', get_chat_title(id, CHANNEL), '\033[0m')
 
 
 def handle_go_to_chat():
@@ -236,12 +246,16 @@ def handle_go_to_chat():
                 temp_dis.add('banned_members')
             if not permissions[6]:
                 temp_dis.add('add_member')
+            if not permissions[6] and chat_type == CHANNEL:
+                temp_dis.add('members')
             if not permissions[7]:
                 temp_dis.add('promote')
             if not permissions[8]:
                 temp_dis.add('update_info')
+            disabled_commands = temp_dis
 
     state = 'inchat_menu'
+    print(f'Chatting with: \033[1;34m{get_chat_title(current_chat_id)}\033[0m!')
 
 
 def handle_leave_chat():
@@ -249,7 +263,22 @@ def handle_leave_chat():
 
 
 def handle_show_messages():
-    pass
+    # variables
+    temp = input('-\tMessage count (default=10): ')
+    limit = int(temp) if temp else 10
+    title_width = 20
+
+    cur.execute("SELECT id from message WHERE destination={current_chat_id} ORDER BY upload_date DESC LIMIT {limit}")
+    messages = cur.fetchall()
+    for message in messages:
+        print(f'*\t{"message_id".center(title_width, " ")}: \033[1;34m{message[1]}\033[0m')
+        print(f'*\t{"date".center(title_width, " ")}: \033[1;34m{message[8]}\033[0m')
+        if message[3]:
+            print(f'*\t{"reply to message_id".center(title_width, " ")}: \033[1;34m{message[5]}\033[0m')
+        if message[2] and message[3]:
+            pass
+
+            print(f'*\t{"forwarded from".center(title_width, " ")}: \033[1;34m{message[5]}\033[0m')
 
 
 def handle_find_message():
@@ -257,7 +286,58 @@ def handle_find_message():
 
 
 def handle_send_message():
-    pass
+    # variables
+    cur.execute(f"SELECT id from message WHERE destination= {current_chat_id}")
+    if cur.fetchone():
+        cur.execute(f"SELECT max(id) from message WHERE destination={current_chat_id}")
+        new_message_id = cur.fetchone()[0] + 1
+    else:
+        new_message_id = 1
+
+    text = 'null'
+    source_chat_id = 'null'
+    source_message_id = 'null'
+    # reply
+    temp = input('-\tReply to message_id (optional): ')
+    if temp:
+        cur.execute(f"SELECT * from message WHERE id={temp} and destination= {current_chat_id}")
+    while not cur.fetchone() and temp:
+        temp = input(
+            '\t\033[0;31mThe message you want to reply to does not exist in this chat, try another message_id (optional): \033[0m').strip()
+        if not temp:
+            break
+        cur.execute(f"SELECT id from message WHERE id={temp} and destination= {current_chat_id}")
+
+    reply_to_id = 'null' if not temp else temp
+    reply_to_chat = 'null' if not temp else current_chat_id
+    temp = input('-\tDo you want to forward an existing message (y/n)? ')
+    if not temp or temp == 'n':
+        temp = input('-\tMesssage text: ')
+        while not temp:
+            temp = input(
+                '\t\033[0;31mMesssage text cannot be empty: \033[0m').strip()
+        text = f"'{temp}'"
+    else:
+        # forwarding message
+        temp = input('-\tForward source chat_id: ')
+        while not temp:
+            temp = input(
+                '\t\033[0;31mSource chat_id cannot be empty: \033[0m').strip()
+        source_chat_id = temp
+
+        temp = input('-\tForward source message_id: ')
+        while not temp:
+            temp = input(
+                '\t\033[0;31mSource message_id cannot be empty: \033[0m').strip()
+        source_message_id = temp
+        cur.execute(f"SELECT * from message WHERE id='{source_message_id}' and destination='{source_chat_id}'")
+        if not cur.fetchone():
+            print(
+                '\t\033[0;31mSource message could not be found\033[0m').strip()
+            return
+    cur.execute(
+        f"INSERT INTO message VALUES ({current_chat_id}, {new_message_id}, {source_chat_id}, {source_message_id}, {'null' if reply_to_id == 'null' else current_chat_id},{reply_to_id}, {user_id}, null,DEFAULT, null,{text})")
+    conn.commit()
 
 
 def handle_chat_info():
@@ -300,26 +380,30 @@ def handle_creating_pv():
 
 
 def handle_creating_gp_ch(is_group):
-    title = input(f"-\tPlease enter the title of your {'group' if is_group else 'channel'}:\t")
-    while not title:
-        print("\tTitle can not be empty.")
+    def wrapper():
         title = input(f"-\tPlease enter the title of your {'group' if is_group else 'channel'}:\t")
-    description = input(f"-\tPlease enter the description of your {'group' if is_group else 'channel'}(optional):\t")
-    is_private = input(f"-\tShall your {'group' if is_group else 'channel'} be private?(y/n):\t")
-    while not is_private:
-        print("\tGroup has to be private or not.")
+        while not title:
+            print("\tTitle can not be empty.")
+            title = input(f"-\tPlease enter the title of your {'group' if is_group else 'channel'}:\t")
+        description = input(
+            f"-\tPlease enter the description of your {'group' if is_group else 'channel'}(optional):\t")
         is_private = input(f"-\tShall your {'group' if is_group else 'channel'} be private?(y/n):\t")
-    user_name = input(f"-\tPlease enter the username of your {'group' if is_group else 'channel'}(optional):\t")
+        while not is_private:
+            print("\tGroup has to be private or not.")
+            is_private = input(f"-\tShall your {'group' if is_group else 'channel'} be private?(y/n):\t")
+        user_name = input(f"-\tPlease enter the username of your {'group' if is_group else 'channel'}(optional):\t")
 
-    gp_id = 0
-    creator = user_id
-    is_private = True if is_private == 'y' else False
-    inv_link = 'chiz.me/' + uuid.uuid4().hex[:10]
-    chat_type = 'G' if is_group else 'C'
+        gp_id = 0
+        creator = user_id
+        is_private = True if is_private == 'y' else False
+        inv_link = 'chiz.me/' + uuid.uuid4().hex[:10]
+        chat_type = 'G' if is_group else 'C'
 
-    cur.execute(
-        f"INSERT into groupchannelchat values({gp_id}, {creator}, {title}, {description}, {is_private}, {inv_link}, {user_name}, {chat_type})")
-    conn.commit()
+        cur.execute(
+            f"INSERT into groupchannelchat values({gp_id}, {creator}, {title}, {description}, {is_private}, {inv_link}, {user_name}, {chat_type})")
+        conn.commit()
+
+    return wrapper
 
 
 # global commands
@@ -367,6 +451,7 @@ chats_menu_commands = {
     'goto': (handle_go_to_chat, 'go to a specific chat'),
     'leave': (handle_leave_chat, 'go to chat menu'),
     'create': (change_menu('create_chat'), 'create a new chat'),
+    'join': (handle_join_chat, 'join an existing chat'),
     'back': (change_menu('menu'), 'back to main menu'),
     'help': (handle_help, 'print available commands'),
 }
@@ -380,7 +465,7 @@ inchat_menu_commands = {
     'banned_members': (change_menu('members_menu'), 'show messages of this chat'),
     'admins': (change_menu('admins_menu'), 'show messages of this chat'),
     'info': (handle_chat_info, 'show information about this chat'),
-    'back': (change_menu('chats'), 'go back to chats menu'),
+    'back': (change_menu('chats_menu'), 'go back to chats menu'),
     'help': (handle_help, 'print available commands'),
 }
 
@@ -421,6 +506,5 @@ def handle_command(command):
 
 if __name__ == '__main__':
     while True:
-        print("Hi")
         handle_state_transitions()
         handle_command(input('$ ').strip())
